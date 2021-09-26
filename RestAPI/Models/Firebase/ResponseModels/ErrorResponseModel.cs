ee2a48709e26dd16fd66c201753d0b1a7d3cf497
Algimantas <grabliauskas.a@gmail.com>
2021-09-26 14:02:37 +0300

    Add Models/Firebase

---
+++ RestAPI/Models/Firebase/ResponseModels/ErrorResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.Models.Firebase.ResponseModels
+{
+    public class ErrorResponseModel
+    {
+    }
+}
