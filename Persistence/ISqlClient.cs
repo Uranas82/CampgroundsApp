8831ca48f513eb251e4d1df6954eec3f1e6e4825
Algimantas <grabliauskas.a@gmail.com>
2021-09-27 20:25:21 +0300

    Added persistence

--- Persistence/ISqlClient.cs
+++ Persistence/ISqlClient.cs
@@ -8,5 +8,10 @@ namespace Persistence
 {
     public interface ISqlClient
     {
+        Task<int> ExecuteAsync(string sql, object param = null);
+
+        Task<IEnumerable<T>> QueryAsync<T>(string sql, object param = null);
+
+        Task<T> QueryFirstOrDefaultAsync<T>(string sql, object param = null);
     }
 }
---
+++ Persistence/Models/ReadModels/CampgroundReadModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Models.ReadModels
+{
+    public class CampgroundReadModel
+    {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public string Url { get; set; }
+    }
+}
