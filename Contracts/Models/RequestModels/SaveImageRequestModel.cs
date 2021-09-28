d4066aa7c4071a58f73f3eaff001a7492385c3e6
Algimantas <grabliauskas.a@gmail.com>
2021-09-28 22:05:52 +0300

    Added rest controllers

---
+++ Contracts/Models/RequestModels/SaveImageRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Models.RequestModels
+{
+    public class SaveImageRequestModel
+    {
+        public Guid CampgroundId { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ Contracts/Models/ResponseModels/SaveImageResponseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Models.ResponseModels
+{
+    public class SaveImageResponseModel
+    {
+        public Guid Id { get; set; }
+
+        public Guid CampgroundId { get; set; }
+
+        public string Url { get; set; }
+    }
+}
